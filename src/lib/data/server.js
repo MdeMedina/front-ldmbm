f335937499b12f0c2beafc10a004afbc9f707a98
MdeMedina <70539248+MdeMedina@users.noreply.github.com>
2025-01-25 00:33:07 -0300

    server view

--- src/lib/data/server.js
+++ src/lib/data/server.js
@@ -1,8 +1,10 @@
 const backendUrl = () => {
+  console.log("BACKEND_URL:", process.env);
   return process.env.BACKEND_URL;
 };
 
 const frontUrl = () => {
+  console.log("FRONTEND_URL:", process.env);
   return process.env.FRONTEND_URL;
 };
 
