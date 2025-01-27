02527253162da6abfd18a6537ca53489657e3193
MdeMedina <70539248+MdeMedina@users.noreply.github.com>
2025-01-27 14:01:03 -0300

    Update server.js

--- src/lib/data/server.js
+++ src/lib/data/server.js
@@ -5,7 +5,7 @@ const backendUrl = () => {
 
 const frontUrl = () => {
   console.log("FRONTEND_URL:", process.env);
-  return process.env.REACT_APP_BACKEND_URL;
+  return process.env.REACT_APP_FRONTEND_URL;
 };
 
 module.exports = {
